353cde0cf5ce69b687310a60f7e5c274a597391c
scrawl <scrawl@email.com>
2026-03-06 02:02:11 +0000

    fix each2binary error cleanup

--- src/iter.c
+++ src/iter.c
@@ -8,7 +8,7 @@ K each2binary(DYAD f, K x, K y){
     K r = knew(KObjType, HDR_COUNT(x)), *robj = OBJ_PTR(r);
     FOR_EACH(r){
         K t = f(item(i, x), item(i, y));
-        if (!t){ while(--i) unref(robj[i]); return UNREF_XY(0); }
+        if (!t){ HDR_COUNT(r)=i; unref(r); return UNREF_XY(0); }
         robj[i] = t;
     }
     return UNREF_XY(r);
